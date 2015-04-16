ddc8d319f7434f81610fd3dd719296245d6d0160
MCHLWRRN <me@mchlwrrn.com>
2015-04-16 14:42:33 -0700

    Hooked up cart create function

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -6,4 +6,16 @@ class CartsController < ApplicationController
 	def new
 		@cart = Cart.new
 	end
+
+	def create
+		Cart.create(cart_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def cart_params
+		params.require(:cart).permit(:name, :description, :address)
+	end
+
 end
