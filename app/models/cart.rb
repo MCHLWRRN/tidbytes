d5885d645a1fc9f7d9ee3a0fbc860ed489b22f75
MCHLWRRN <me@mchlwrrn.com>
2015-04-20 16:52:23 -0700

    Added more validations

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,7 @@
 class Cart < ActiveRecord::Base
 	belongs_to :user
-	validates :name, :presence => true
+	validates :name, :presence => true, :length => { :minimum => 3 }
+	validates :address, :presence => true,
+	validates :description, :presence => true
+
 end
