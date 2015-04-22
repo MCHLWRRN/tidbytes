7cab55e636ebac6d4f3448063dd43477c844da5d
MCHLWRRN <me@mchlwrrn.com>
2015-04-22 16:07:49 -0700

    Fixing migration data

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,10 @@
 class CommentsController < ApplicationController
 	before_action :authenticate_user!
+	
 	def create
 		@cart = Cart.find(params[:cart_id])
 		@cart.comments.create(comment_params.merge(:user => current_user))
-		redurect_to cart_path(@cart)
+		redirect_to cart_path(@cart)
 	end
 
 	private
--- db/migrate/20150421212516_create_comments.rb
+++ db/migrate/20150421212516_create_comments.rb
@@ -5,11 +5,11 @@ class CreateComments < ActiveRecord::Migration
     	t.string :rating
 
     	t.integer :user_id
-    	t.integer :place_id
+    	t.integer :cart_id
       t.timestamps
     end
 
-    add_index :comments, [:user_id, :place_id]
-    add_index :comments, :place_id
+    add_index :comments, [:user_id, :cart_id]
+    add_index :comments, :cart_id
   end
 end
